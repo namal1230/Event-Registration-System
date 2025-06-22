541f7346b3d2739e1a6224f5369fe003e93eaa76
Namal Dilmith <namaldilmith2@gmail.com>
2025-06-22 15:33:58 +0530

    create login page

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="***">
+        </div>
+        <button type="submit" class="btn btn-primary">SignIn</button>
+        <button type="button" class="btn btn-secondary" onclick="window.location.href='register.php'">Register</button>
+    </form>
+</body>
+</html>
